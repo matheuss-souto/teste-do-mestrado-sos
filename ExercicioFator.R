0d4a2782d56beded44bbd52c7602cfedbbdcae7a
matheuss-souto <131391682+matheuss-souto@users.noreply.github.com>
2023-05-29 18:19:39 -0300

    Add files via upload

---
+++ ExercicioFator.R
@@ -0,0 +1,11 @@
+frutas <- c("Maçã", "Banana", "Laranja", "Maçã", "Uva", "Banana", "Maçã")
+
+fator_frutas <- factor(frutas)
+
+print(fator_frutas)
+
+#resultado:
+
+#[1] Maçã    Banana  Laranja Maçã    Uva     Banana 
+#[7] Maçã   
+#Levels: Banana Laranja Maçã Uva
